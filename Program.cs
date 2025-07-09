d728dcb2e172e1440ad294b70ac73d0cd8bf9c4f
Bắp <117089665+Nguyenne094@users.noreply.github.com>
2025-07-09 11:40:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿class HelloWorld {
+  static void Main()
+  {
+    Dictionary<string, string> telexRule = new()
+    {
+      { "aw", "ă" },
+      { "aa", "â" },
+      { "dd", "đ" },
+      { "ee", "ê" },
+      { "oo", "ô" },
+      { "ow", "ơ" },
+      { "w", "ư" }
+    };
+    List<string> matchedTelexKeys = new();
+    Console.Write("Enter character string: ");
+    string? input = Console.ReadLine();
+    var lowerInput = input.ToLower();
+    int i = 0;
+    while (i < lowerInput.Length)
+    {
+      bool matched = false;
+      
+      if (i + 1 < lowerInput.Length)
+      {
+        string twoChar = lowerInput.Substring(i, 2);
+        if (telexRule.ContainsKey(twoChar))
+        {
+          matchedTelexKeys.Add(twoChar);
+          i += 2;
+          matched = true;
+        }
+      }
+
+      if (!matched)
+      {
+        string oneChar = lowerInput.Substring(i, 1);
+        if (telexRule.ContainsKey(oneChar))
+        {
+          matchedTelexKeys.Add(oneChar);
+        }
+      }
+      i += 1;
+    }
+
+    Console.WriteLine(matchedTelexKeys.Count + ": (" + string.Join(", ", matchedTelexKeys) + ")");
+  }
+}
